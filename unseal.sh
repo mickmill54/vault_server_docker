e4a55023eef3d69f62df95544ecf4e2c388713bc
Mick Miller <mick@mickmill.com>
2020-12-21 22:11:46 -0500

    updated .gitignore

---
+++ unseal.sh
@@ -0,0 +1,7 @@
+# Unseal
+vault operator unseal Y9ubijAm4kq83CSx4NqXs+0NRjCcIAwcY+22l6RHDXTZ
+vault operator unseal 7UZH5mFiRJGvwOFwCDbh37br05FME8nUPKBY55FRcugn
+vault operator unseal FkBxSdUQ20/7PeqLwYupru2GoCs2i+f5u48LeCIwPj7K
+
+# Login
+vault login s.LwyIsILu0qR0I5UFtrJlW3ev
