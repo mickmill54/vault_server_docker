52e6ed68ac5ff2c4c3d00a17a791833fe3bba229
Mick Miller <mick@mickmill.com>
2020-12-31 00:17:14 -0500

    remove terraform state amd init files

--- cleanup.sh
+++ cleanup.sh
@@ -1,9 +1,13 @@
 # Remove all terraform states
 cd terraform
 terraform destroy -auto-approve
+rm -rf .terraform
+rm -f terraform.tfstate
+rm -f terraform.tfstate.backup
+cd ..
 
 # Shutdown docker
-docker-compose down
+docker-compose down --volumes
 
 # Delete the volumes
 rm -rf ~/volumes
